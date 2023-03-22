8414746d7e5455d88c30a32b4e93525dc2a17c50
Debury17 <svipbighead67@gmail.com>
2023-03-22 22:54:04 +0800

    feature:welcome 第一个页面

---
+++ src/components/welcome/First.tsx
@@ -0,0 +1,24 @@
+import { defineComponent, ref } from "vue";
+import {RouterLink} from "vue-router"
+import pig from "../../assets/svg/pig.svg"
+import s from "./First.module.scss"
+export const First = defineComponent({
+  setup(props, context) {
+    return () => (
+      
+        <div class={s.wrapper}>
+          <div class={s.card}>
+            <img src={pig} alt="" />
+            <h2>会挣钱 <br /> 还会省钱</h2>
+          </div>
+          <div class={s.actions}>
+            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+            <RouterLink to="/welcome/2">下一页</RouterLink>
+            <RouterLink to="/start">跳过</RouterLink>
+
+          </div>
+        </div>
+      
+    );
+  },
+});
