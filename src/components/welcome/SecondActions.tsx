b8a647f008cc0c57bb98045c5545fb507af6cdc2
Debury17 <svipbighead67@gmail.com>
2023-03-24 22:56:04 +0800

    footer 多重routerview

---
+++ src/components/welcome/SecondActions.tsx
@@ -0,0 +1,15 @@
+import {  FunctionalComponent, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+import s from "./welcome.module.scss";
+
+
+export const SecondActions :FunctionalComponent = (props,context)=>{
+    return <div class={s.actions}>
+            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+          <RouterLink to="/welcome/3">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+    </div>
+}
+
+SecondActions.displayName = "Second"
