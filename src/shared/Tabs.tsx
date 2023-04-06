58a55f9bf6abbdf8f0e25ac3ccdc1a165c055741
Debury17 <svipbighead67@gmail.com>
2023-04-06 22:41:48 +0800

    扩展tabs组件 支持classPrefix属性选择

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -4,6 +4,9 @@ import { ItemList } from "../components/item/ItemList";
 import s from "./Tabs.module.scss";
 export const Tabs = defineComponent({
   props: {
+    classPrefix:{
+      type:String
+    },
     selected: {
       type: String as PropType<string>,
       required:false
@@ -17,18 +20,20 @@ export const Tabs = defineComponent({
   setup: (props, context) => {
     return () => {
       const array = context.slots.default?.();
-    //   console.log(array)
       if (!array) return () => null;
       array.forEach((value) => {
-        
         if (value.type !== Tab) {
           throw new Error("<Tabs> only accept <Tab> as children");
         }
       });
-      return <div class={s.tabs}>
+      const cp = props.classPrefix
+      return <div class={[s.tabs,cp+"_tabs"]}>
                   
-                    <ol class={s.tabs_nav}>
-                        {array.map(item => <li class={item.props?.name === props.selected ? s.selected : ''}
+                    <ol class={[s.tabs_nav,cp+"_tabs_nav"]}>
+                        {array.map(item => <li class={[
+                          item.props?.name === props.selected ? [s.selected,cp + "_selected"] : '',
+                          cp + '_tabs_nav_item'
+                        ]}
                             onClick={()=>context.emit('update:selected',item.props?.name)}
                         >
                             {item.props?.name}</li>)}
