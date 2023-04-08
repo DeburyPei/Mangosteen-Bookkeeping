eca7c314160307c5fa37d248368a17a1470f0d7f
Debury17 <svipbighead67@gmail.com>
2023-04-08 22:34:14 +0800

    学习v-model新用法然后更新自定义时间只能点一次的错误

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -30,22 +30,23 @@ export const ItemList = defineComponent({
          end:time.lastDayOfYear(),
       }
     ]
-    watchEffect(() => {
-      if (refSelected.value === '自定义时间') {
-        refOverlayVisible.value = true
-      }
-    })
+   
     const refOverlayVisible = ref(false)
     const onSubmitCustomTime = (e: Event) => {
       e.preventDefault()
-      refOverlayVisible.value = false
+      refOverlayVisible.value=false
+    }
+    const onSelect = (value:string)=>{
+         if(value==="自定义时间") refOverlayVisible.value=true
     }
     return () => (
          <MainLayout>{{
             title:()=>"山竹记账",
             icon:()=> <Icon name='menu'/>,
             default:()=><>
-               <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+               <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}
+               onUpdate:selected={onSelect}
+               >
                   <Tab name="本月">
                   <ItemSummary
                      startDate={timeList[0].start.format()}
@@ -88,7 +89,7 @@ export const ItemList = defineComponent({
                               />
                              <FormItem>
                               <div class={s.actions}>
-                                 <button type="button">取消</button>
+                                 <button type="button" onClick={()=>refOverlayVisible.value=false}>取消</button>
                                  <button type="submit" onClick={onSubmitCustomTime}>确认</button>
                               </div>
                              </FormItem>
