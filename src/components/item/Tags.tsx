c17b63ee50e3101bfa2289780be1a6d8b50004e7
Debury17 <svipbighead67@gmail.com>
2023-06-19 17:02:37 +0800

    长按跳转router push

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, onUpdated, PropType, ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRouter } from 'vue-router';
 import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
@@ -26,15 +26,18 @@ export const Tags = defineComponent({
     const onSelect = (tag:Tag) =>{
         context.emit('update:selected',tag.id)
     }
+
     const timer = ref<number>()
     const currentTag = ref<HTMLDivElement>()
-    const onLongPress = () => {
-      console.log("长按")
+
+    const router = useRouter()
+    const onLongPress = (tagId:Tag['id']) => {
+      router.push(`/tags/${tagId}/edit?kind=${props.kind}&return_to=${router.currentRoute.value.fullPath}`)
     }
-    const onTouchStart = (e:TouchEvent) => {
+    const onTouchStart = (e:TouchEvent,tag:Tag) => {
       currentTag.value = e.currentTarget as HTMLDivElement
       timer.value = setTimeout(()=>{
-        onLongPress()
+        onLongPress(tag.id)
       },500)
     }
     const onTouchEnd = (e:TouchEvent)=>{
@@ -44,7 +47,6 @@ export const Tags = defineComponent({
       const pointedElement = document.elementFromPoint(e.touches[0].clientX,e.touches[0].clientY)
       if(currentTag.value !== pointedElement && currentTag.value?.contains(pointedElement)===false){
         clearTimeout(timer.value)
-
       }
     }
     return () => <>
@@ -60,7 +62,7 @@ export const Tags = defineComponent({
         {tags.value.map(tag =>
           <div class={[s.tag, props.selected === tag.id ? s.selected:'']}
             onClick={()=>onSelect(tag)}
-            onTouchstart = {onTouchStart}
+            onTouchstart = {(e)=>onTouchStart(e,tag)}
             onTouchend={onTouchEnd}
           >
             <div class={s.sign}>
