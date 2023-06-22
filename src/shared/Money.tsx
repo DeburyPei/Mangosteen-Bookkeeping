204e2391fe794c2a8884b42403a868477c57f8cb
Debury17 <svipbighead67@gmail.com>
2023-06-22 16:07:33 +0800

    折线图

--- src/shared/Money.tsx
+++ src/shared/Money.tsx
@@ -24,3 +24,18 @@ export const Money = defineComponent({
       )
     }
 })
+
+const addZero = (n: number) => {
+    const nString = n.toString()
+    const dotIndex = nString.indexOf('.')
+    if (dotIndex < 0) {
+      return nString + '.00'
+    } else if (nString.substring(dotIndex).length === 2) {
+      return nString + '0'
+    } else {
+      return nString
+    }
+  }
+  export const getMoney = (n: number) => {
+    return addZero(n / 100)
+  }
